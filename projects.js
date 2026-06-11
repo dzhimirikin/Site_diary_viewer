47ed5c8b15e339935308ae902511c33fe8cb4ac2
dzhimirikin <d.zhimirikin@gmail.com>
2026-06-11 21:59:21 +0300

    Update diary

---
+++ projects.js
@@ -0,0 +1,33 @@
+const projects = [
+    "ERR",
+    "TEST1",
+    "TEST2",
+    "111",
+    "jkljkljlk"
+];
+
+const container =
+    document.getElementById("projects");
+
+for (const project of projects) {
+
+    const link =
+        document.createElement("a");
+
+    link.href =
+        `${project}/`;
+
+    link.textContent =
+        project;
+
+    link.style.display =
+        "block";
+
+    link.style.margin =
+        "20px";
+
+    link.style.fontSize =
+        "24px";
+
+    container.appendChild(link);
+}
