ed92286fec9e314eb6dc59da1731a6ee68a4d9db
dzhimirikin <d.zhimirikin@gmail.com>
2026-06-30 20:08:16 +0300

    Update diary

--- TEST PROJECT/gallery.js
+++ TEST PROJECT/gallery.js
@@ -1640,57 +1640,14 @@ let currentTextarea =
 
 function showPhoto() {
 
-    const image =
-
-        document.getElementById(
+    document
+        .getElementById(
             "viewer-image"
-        );
-
-    image.src =
-        currentPhotos[
-            currentIndex
-        ];
-
-    //--------------------------------------------------
-    // Inspector
-    //--------------------------------------------------
-
-    const path =
-
-        currentPhotos[
-            currentIndex
-        ];
-
-    const parts =
-
-        path.split("/");
-
-    const file =
-
-        parts[
-            parts.length - 1
-        ];
-
-    const day =
-
-        parts[
-            parts.length - 3
-        ];
-
-    document
-        .getElementById(
-            "info-file"
         )
-        .textContent =
-            file;
-
-    document
-        .getElementById(
-            "info-date"
-        )
-        .textContent =
-            day;
-
+        .src =
+            currentPhotos[
+                currentIndex
+            ];
 }
 
 function openViewer(
@@ -1705,16 +1662,6 @@ function openViewer(
         index;
 
     showPhoto();
-
-    const info =
-        document.getElementById(
-            "viewer-info"
-        );
-
-    info.style.display =
-        currentMode === "layout"
-            ? "block"
-            : "none";
 
     document
         .getElementById(
